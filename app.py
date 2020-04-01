64614b7117e1fd5e43c7273fe37868c415b78031
Tomasz Knapik <hi@tmkn.org>
2020-04-01 22:09:37 +0100

    use pyplot

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import csv
 import io
+import tempfile
 
-from flask import Flask, jsonify, make_response, render_template, request
+from flask import (
+    abort,
+    Flask,
+    jsonify,
+    make_response,
+    render_template,
+    request,
+    redirect,
+    url_for,
+)
+import matplotlib.pyplot as plt
 
 from gillespie import gillespie
 
@@ -13,8 +24,8 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/data/")
-def gillespie_data():
+@app.route("/export-json/")
+def export_json():
     data = list(
         gillespie(
             population=int(request.args.get("population", 350)),
@@ -47,7 +58,7 @@ def export_csv():
             infected_population=int(request.args.get("infected_population", 1)),
         )
     )
-    csv_file = io.StringIO()
+    csv_file = tempfile.TemporaryFile(mode="r+")
     dict_writer = csv.DictWriter(
         csv_file, fieldnames=["Time", "Susceptible", "Infected", "Recovered"]
     )
@@ -61,7 +72,67 @@ def export_csv():
                 "Recovered": point.recovered_population,
             }
         )
-    response = make_response(csv_file.getvalue())
+    csv_file.seek(0)
+    response = make_response(csv_file.read())
+    csv_file.close()
     response.headers["content-type"] = "text/csv"
     response.headers["content-disposition"] = "attachment; filename=gillespie.csv"
+    return response
+
+
+@app.route("/export-graph/")
+def default_graph_export():
+    return redirect(url_for("export_graph", format="svg"))
+
+
+@app.route("/export-graph/<format>/")
+def export_graph(format="svg"):
+    if format not in ("svg", "png"):
+        return abort(400, "Invalid format requested")
+    data = list(
+        gillespie(
+            population=int(request.args.get("population", 350)),
+            maximum_elapsed_time=float(request.args.get("maximum_elapsed_time", 1000)),
+            start_time=float(request.args.get("start_time", 0.0)),
+            spatial_parameter=float(request.args.get("spatial_parameter", 100.0)),
+            rate_of_infection_after_contact=float(
+                request.args.get("rate_of_infection_after_contact", 10.0)
+            ),
+            rate_of_cure=float(request.args.get("rate_of_cure", 0.5)),
+            infected_population=int(request.args.get("infected_population", 1)),
+        )
+    )
+    plt.clf()
+    plt.plot(
+        [x.time for x in data],
+        [x.susceptible_population for x in data],
+        "b",
+        label="Susceptible",
+    )
+    plt.plot(
+        [x.time for x in data],
+        [x.infected_population for x in data],
+        "r",
+        label="Infected",
+    )
+    plt.plot(
+        [x.time for x in data],
+        [x.recovered_population for x in data],
+        "g",
+        label="Recovered",
+    )
+    plt.xlabel("Tme units (generic) log scale")
+    plt.ylabel("Cases from 1 index case")
+    plt.title("Susceptible, Infected and Recovered - cases vs. log (time)")
+    plt.legend()
+    file_object = tempfile.TemporaryFile()
+    plt.savefig(file_object, dpi=300, format=format, orientation="landscape")
+    plt.clf()
+    file_object.seek(0)
+    response = make_response(file_object.read())
+    file_object.close()
+    if format == "png":
+        response.headers["content-type"] = "image/png"
+    elif format == "svg":
+        response.headers["content-type"] = "image/svg+xml"
     return response
--- static_src/js/index.js
+++ static_src/js/index.js
@@ -1,9 +1,6 @@
 import "whatwg-fetch";
 import "url-polyfill";
 import "@babel/polyfill";
-import Highcharts from "highcharts";
-import HighchartsExporting from "highcharts/modules/exporting";
-HighchartsExporting(Highcharts);
 
 function getFormData() {
   return {
@@ -19,7 +16,28 @@ function getFormData() {
   };
 }
 
-async function fetchData({
+function getUrlParamsFromFormData() {
+  const params = new URLSearchParams();
+  const {
+    population,
+    maximumElapsedTime,
+    startTime,
+    spatialParameter,
+    rateOfInfectionAfterContact,
+    rateOfCure,
+    infectedPopulation,
+  } = getFormData();
+  params.set("population", population);
+  params.set("maximum_elapsed_time", maximumElapsedTime);
+  params.set("start_time", startTime);
+  params.set("spatial_parameter", spatialParameter);
+  params.set("rate_of_infection_after_contact", rateOfInfectionAfterContact);
+  params.set("rate_of_cure", rateOfCure);
+  params.set("infected_population", infectedPopulation);
+  return params;
+}
+
+function getChartUrl({
   population,
   maximumElapsedTime,
   startTime,
@@ -28,103 +46,15 @@ async function fetchData({
   rateOfCure,
   infectedPopulation,
 }) {
-  const params = new URLSearchParams();
-  params.set("population", population);
-  params.set("maximum_elapsed_time", maximumElapsedTime);
-  params.set("start_time", startTime);
-  params.set("spatial_parameter", spatialParameter);
-  params.set("rate_of_infection_after_contact", rateOfInfectionAfterContact);
-  params.set("rate_of_cure", rateOfCure);
-  params.set("infected_population", infectedPopulation);
-  const response = await fetch(`/data/?${params}`, {
-    headers: {
-      accept: "application/json",
-    },
-  });
-  if (!response.ok) {
-    throw new Error(`Unexpected HTTP Error: ${response.statusText}`);
-  }
-  return await response.json();
+  return `/export-graph/png/?${getUrlParamsFromFormData()}`;
 }
 
-async function loadChart() {
+function loadChart() {
   const chartContainer = document.querySelector("#chart-container");
-  const data = await fetchData(getFormData());
-
-  Highcharts.chart(chartContainer, {
-    title: {
-      text: "Susceptible, Infected and Recovered - cases vs. log (time)",
-    },
-
-    subtitle: {
-      text: "SIR model - Gillespie algorithm solution",
-    },
-
-    yAxis: {
-      title: {
-        text: "Cases from 1 index case",
-      },
-    },
-
-    xAxis: {
-      title: {
-        text: "Tme units (generic) log scale",
-      },
-    },
-
-    legend: {
-      layout: "vertical",
-      align: "right",
-      verticalAlign: "middle",
-    },
-
-    plotOptions: {
-      series: {
-        turboThreshold: 0,
-      },
-    },
-
-    series: [
-      {
-        name: "Susceptible",
-        data: data.map(({ susceptible_population, time }) => ({
-          y: susceptible_population,
-          x: time,
-        })),
-      },
-      {
-        name: "Infected",
-        data: data.map(({ infected_population, time }) => ({
-          y: infected_population,
-          x: time,
-        })),
-      },
-      {
-        name: "Recovered",
-        data: data.map(({ recovered_population, time }) => ({
-          y: recovered_population,
-          x: time,
-        })),
-      },
-    ],
-
-    responsive: {
-      rules: [
-        {
-          condition: {
-            maxWidth: 500,
-          },
-          chartOptions: {
-            legend: {
-              layout: "horizontal",
-              align: "center",
-              verticalAlign: "bottom",
-            },
-          },
-        },
-      ],
-    },
-  });
+  chartContainer.innerHTML = "";
+  const imageTag = document.createElement("img");
+  imageTag.src = getChartUrl(getFormData());
+  chartContainer.appendChild(imageTag);
 }
 
 function setUpForm() {
@@ -138,24 +68,21 @@ function setUpForm() {
 function setUpDownloadCSVButton() {
   const button = document.querySelector("#download-csv-button");
   button.addEventListener("click", () => {
-    const params = new URLSearchParams();
-    const {
-      population,
-      maximumElapsedTime,
-      startTime,
-      spatialParameter,
-      rateOfInfectionAfterContact,
-      rateOfCure,
-      infectedPopulation,
-    } = getFormData();
-    params.set("population", population);
-    params.set("maximum_elapsed_time", maximumElapsedTime);
-    params.set("start_time", startTime);
-    params.set("spatial_parameter", spatialParameter);
-    params.set("rate_of_infection_after_contact", rateOfInfectionAfterContact);
-    params.set("rate_of_cure", rateOfCure);
-    params.set("infected_population", infectedPopulation);
-    window.open(`/export-csv/?${params}`);
+    window.open(`/export-csv/?${getUrlParamsFromFormData()}`);
+  });
+}
+
+function setUpDownloadJSONButton() {
+  const button = document.querySelector("#download-json-button");
+  button.addEventListener("click", () => {
+    window.open(`/export-json/?${getUrlParamsFromFormData()}`);
+  });
+}
+
+function setUpDownloadSVGButton() {
+  const button = document.querySelector("#download-svg-button");
+  button.addEventListener("click", () => {
+    window.open(`/export-graph/svg/?${getUrlParamsFromFormData()}`);
   });
 }
 
@@ -163,4 +90,6 @@ window.addEventListener("DOMContentLoaded", () => {
   loadChart();
   setUpForm();
   setUpDownloadCSVButton();
+  setUpDownloadJSONButton();
+  setUpDownloadSVGButton();
 });
